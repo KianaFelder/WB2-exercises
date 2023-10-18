1b77cc9f5f36b1164c69cd09da69d0372a6316ec
KianaFelder <Kianafelder96@gmail.com>
2023-10-17 23:15:58 -0400

    Q1

---
+++ MathScripts/net_worth.js
@@ -0,0 +1,11 @@
+"usestrict";
+
+var assets = 2633;
+var debt = 883;
+
+var netWorth = 2633 - 883;
+
+console.log('Your net worth is' + netWorth)
+
+
+
--- tips.js
+++ tips.js
@@ -9,4 +9,7 @@ var tip = 12.00;
 var totalDue = foodCost + tax + tip;
 
 //output
-console.log("The total due is " + totalDue);
+console.log("The food cost is " + foodCost + " and tax is " + tax);
+console.log("Tip is " + tip.toFixed(2));
+console.log("Total due is " + totalDue);
+
