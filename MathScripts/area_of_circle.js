c72d79c6346491d588c043d872293c6b31c3727a
KianaFelder <Kianafelder96@gmail.com>
2023-10-18 00:48:24 -0400

    Challenge 5

---
+++ MathScripts/area_of_circle.js
@@ -0,0 +1,10 @@
+"usestrict";
+// sample inputs
+var radius = 9;
+var pi = 3.14;
+
+//calculations
+var area = pi * radius * radius;
+
+//output
+console.log("The area of the cicle is " + area);
---
+++ MathScripts/rule_of_72.js
@@ -0,0 +1,8 @@
+"usestrict";
+ var interestRate = 5;
+ var years = 72
+ var savingAccount = 76445;
+
+ var 
+
+ console.log("The interest rate is "
